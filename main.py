93d6ea78b710b3ce43589f42fab7fb060034fc51
Muxel <41229495+muxe1@users.noreply.github.com>
2023-11-24 18:10:47 +0300

    init

---
+++ main.py
@@ -0,0 +1,24 @@
+import sys
+from proxmuxel import Proxmuxel
+
+match sys.argv[1]:
+    case "start":
+        start_by = int(sys.argv[2])
+        end_by = int(sys.argv[3])
+        Proxmuxel.containers_range_start(start_by, end_by)
+        
+    case "stop":
+        start_by = int(sys.argv[2])
+        end_by = int(sys.argv[3])
+        Proxmuxel.containers_range_stop(start_by, end_by)    
+
+    case "bash":
+        command = int(sys.argv[2])
+        start_by = int(sys.argv[3])
+        end_by = int(sys.argv[4])
+        Proxmuxel.containers_range_runnig_bash(start_by, end_by, command)    
+        
+    case _:
+        raise "Такой команды нет"    
+        
+print("Success")
---
+++ proxmuxel.py
@@ -0,0 +1,18 @@
+import os
+
+class Proxmuxel:   
+    def containers_range_start(start_by: int, end_by: int):
+        for i in range(start_by, end_by+1):
+            print(i)
+            os.system(f"start stop {i}")
+        
+    def containers_range_stop(start_by: int, end_by: int):    
+        for i in range(start_by, end_by+1):
+            print(i)
+            os.system(f"pct stop {i}")
+    
+    def containers_range_runnig_bash(start_by: int, end_by: int, command: str):    
+        for i in range(start_by, end_by+1):
+            print(i)
+            os.system(f"pct exec {i} {command}")
+            
