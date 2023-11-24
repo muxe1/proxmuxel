8d26b25ab9549115910bd2516e97503ceeee341b
Muxel <41229495+muxe1@users.noreply.github.com>
2023-11-24 18:20:07 +0300

    init

--- proxmuxel.py
+++ proxmuxel.py
@@ -2,7 +2,9 @@ import os
 
 class Proxmuxel:   
     def containers_range_start(start_by: int, end_by: int):
-        os.system(f"pct start {i}")
+        for i in range(start_by, end_by+1):
+            print(i)
+            os.system(f"pct start {i}")
         
     def containers_range_stop(start_by: int, end_by: int):    
         for i in range(start_by, end_by+1):
